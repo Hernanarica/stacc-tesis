882109676b710dbce86a73f27fdda1f844925410
Hernán Arica <hernanaricammm@gmail.com>
2022-08-24 22:45:04 -0300

    Relation: Se agrega la fk de user a posts

--- database/migrations/2022_08_24_184409_create_posts_table.php
+++ database/migrations/2022_08_24_184409_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
 	{
 		Schema::create('posts', function (Blueprint $table) {
 			$table->id();
+			$table->foreignId('user_id')->constrained('users')->onDelete('cascade');
 			$table->text('comment');
 			$table->boolean('is_active')->default(0);
 			$table->timestamps();
