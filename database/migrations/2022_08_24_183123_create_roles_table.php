5dd445450d4e951bc1b993353e4f58e8ba0184a0
beto <56642267+be-tho@users.noreply.github.com>
2022-08-24 21:13:48 -0300

    Update 2022_08_24_183123_create_roles_table.php
    remove character limitation

--- database/migrations/2022_08_24_183123_create_roles_table.php
+++ database/migrations/2022_08_24_183123_create_roles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
 	{
 		Schema::create('roles', function (Blueprint $table) {
 			$table->id();
-			$table->string('name', 8);
+			$table->string('name');
 			$table->timestamps();
 		});
 	}
