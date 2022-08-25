48b9cbbc9424c58c25fd04567cf61198c2333f43
Hernán Arica <hernanaricammm@gmail.com>
2022-08-24 21:59:29 -0300

    Template: Se crean las rutas y vistas iniciales junto a sus controllers

---
+++ resources/views/sections/locals.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.layout')
+@section('title', 'Locales')
+@section('content')
+	<section>
+		<h2>Locals</h2>
+	</section>
+@endsection
---
+++ resources/views/sections/profile.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.layout')
+@section('title', 'Mi perfil')
+@section('content')
+	<section>
+		<h2>Profile</h2>
+	</section>
+@endsection
