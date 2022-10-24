4b19e26eef11340a593a62533f372c89e747e883
beto <56642267+be-tho@users.noreply.github.com>
2022-10-23 23:50:15 -0300

    relationship user to locals and post

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,24 @@ class User extends Authenticatable
 	protected $casts = [
 		'email_verified_at' => 'datetime',
 	];
+	
+	/**
+	 * > The `locals()` function returns all the `Local`s that belong to the `User`
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany collection of Local objects.
+	 */
+	public function local()
+	{
+		return $this->hasMany(Local::class);
+	}
+	
+	/**
+	 * The posts() function returns all the posts that belong to the user
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany collection of Post objects.
+	 */
+	public function post()
+	{
+		return $this->hasMany(Post::class);
+	}
 }
