38088d82b8a5fb6d269217ab5b70fc44afebba7f
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-10-28 10:17:00 -0300

    se agregan las configuraciones de fuentes y colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './resources/views/**/*.blade.php'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		'./resources/views/**/*.blade.php'
+	],
+	theme: {
+		extend: {
+			colors: {
+				'stacc-red': '#fa1d37',
+				'stacc-purple': '#941b7f'
+			},
+			fontFamily: {
+				'quicksand': [ 'Quicksand' ],
+				'raleway': [ 'Raleway' ]
+			}
+		}
+	},
+	plugins: []
+};
