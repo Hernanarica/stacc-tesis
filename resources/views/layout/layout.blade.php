df0a3d75058ec1a47dafa77b0a052466f824a48c
Hernán Arica <44417976+Hernanarica@users.noreply.github.com>
2022-11-19 12:16:51 -0300

    ref: se refactoriza el navbar para el usuario admin/visitor, s emejora tambien la ui del navbar

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -10,134 +10,154 @@
 		<title>Stacc | @yield('title', 'Bienvenido')</title>
 	</head>
 	<body>
-		<div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-			<div class="relative flex items-center justify-between">
-				<div class="flex items-center">
-					<a href="/" aria-label="Company" title="Company" class="inline-flex items-center mr-8">
-						<img
-							src="{{ asset('src/assets/images/logos/logo_red.png') }}"
-							alt="stacc logo"
-							width="80"
+		
+		<header class="p-2 py-4 md:px-0">
+			<x-wrapper>
+				<navbar class="relative flex items-center justify-between">
+					<div class="flex items-center">
+						<a href="{{ route('home.index') }}"
+						   aria-label="Company"
+						   title="Company"
+						   class="inline-flex items-center mr-8"
 						>
-					</a>
-					<ul class="flex items-center hidden space-x-8 lg:flex">
-						<li>
-							<a
-								href="{{ route('home.index') }}"
-								aria-label="Our product"
-								title="Home"
-								class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-							>Home</a>
-						</li>
-						<li>
-							<a
-								href="{{ route('locals.index') }}"
-								aria-label="Our product"
-								title="Locales"
-								class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-							>Locales</a>
-						</li>
-						<li>
-							<a
-								href="{{ route('contact.index') }}"
-								aria-label="Product pricing"
-								title="Contacto"
-								class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-							>Contacto</a>
-						</li>
-						@auth()
-						<li>
-							<a href="{{ route('logout.index') }}">Logout</a>
-						</li>
-						<li>
-							<a href="{{ route('favorite.index') }}">Mis favoritos</a>
-						</li>
-						<li>
-							<a href="{{ route('locals.create') }}">Crear local</a>
-						</li>
-						@endauth
-					</ul>
-				</div>
-				<ul class="flex items-center hidden space-x-8 lg:flex">
-					<li>
-						<a
-							href="{{ route('login.index') }}"
-							aria-label="Iniciar sesion"
-							title="Iniciar sesion"
-							class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-						>Iniciar sesion</a>
-					</li>
-					<li>
-						<a
-							href="{{ route('register.create') }}"
-							class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-gradient-to-r from-stacc-purple to-stacc-red focus:shadow-outline focus:outline-none"
-							aria-label="Register"
-							title="Register"
-						>
-							Registrate
+							<img
+								src="{{ asset('src/assets/images/logos/logo_red.png') }}"
+								alt="stacc logo"
+								width="70"
+							>
 						</a>
-					</li>
-				</ul>
-				<!-- Mobile menu -->
-				<div class="lg:hidden">
-					<button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50">
-						<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-							<path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
-							<path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
-							<path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
-						</svg>
-					</button>
-					<!-- Mobile menu dropdown
-					<div class="absolute top-0 left-0 w-full">
-						<div class="p-5 bg-white border rounded shadow-sm">
-							<div class="flex items-center justify-between mb-4">
-								<div>
-									<a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
-										<svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
-											<rect x="3" y="1" width="7" height="12"></rect>
-											<rect x="3" y="17" width="7" height="6"></rect>
-											<rect x="14" y="1" width="7" height="6"></rect>
-											<rect x="14" y="11" width="7" height="12"></rect>
-										</svg>
-										<span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
-									</a>
-								</div>
-								<div>
-									<button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-										<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-											<path
-												fill="currentColor"
-												d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-											></path>
-										</svg>
-									</button>
-								</div>
-							</div>
-							<nav>
-								<ul class="space-y-4">
-									<li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
-									<li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
-									<li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
-									<li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
-									<li><a href="/" aria-label="Sign in" title="Sign in" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Sign in</a></li>
-									<li>
-										<a
-											href="/"
-											class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-											aria-label="Sign up"
-											title="Sign up"
-										>
-											Sign up
-										</a>
-									</li>
-								</ul>
-							</nav>
-						</div>
+						<ul class="flex items-center hidden space-x-8 lg:flex">
+							<li>
+								<a
+									href="{{ route('home.index') }}"
+									aria-label="Our product"
+									title="Home"
+									class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+								>Home</a>
+							</li>
+							<li>
+								<a
+									href="{{ route('locals.index') }}"
+									aria-label="Our product"
+									title="Locales"
+									class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+								>Locales</a>
+							</li>
+							<li>
+								<a
+									href="{{ route('contact.index') }}"
+									aria-label="Product pricing"
+									title="Contacto"
+									class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+								>Contacto</a>
+							</li>
+							@auth()
+								<li>
+									<a href="{{ route('favorite.index') }}">Mis favoritos</a>
+								</li>
+							@endauth
+							@role('admin')
+								<li>
+									<a href="{{ route('locals.create') }}">Crear local</a>
+								</li>
+							@endrole
+						</ul>
 					</div>
-					-->
-				</div>
-			</div>
-		</div>
+					@guest()
+						<ul class="flex items-center hidden space-x-8 lg:flex">
+							<li>
+								<a
+									href="{{ route('login.index') }}"
+									aria-label="Iniciar sesion"
+									title="Iniciar sesion"
+									class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+								>Iniciar sesion</a>
+							</li>
+							<li>
+								<a
+									href="{{ route('register.create') }}"
+									class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-gradient-to-r from-stacc-purple to-stacc-red focus:shadow-outline focus:outline-none"
+									aria-label="Register"
+									title="Register"
+								>
+									Registrate
+								</a>
+							</li>
+						</ul>
+					@endguest
+					@auth()
+						<form action="{{ route('logout.index') }}" method="post">
+							@csrf
+							<button type="submit" class="flex items-center gap-2">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+								</svg>
+								Logout
+							</button>
+						</form>
+					@endauth
+					<!-- Mobile menu -->
+					<div class="lg:hidden">
+						<button aria-label="Open Menu" title="Open Menu" class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50">
+							<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+								<path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"></path>
+								<path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"></path>
+								<path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"></path>
+							</svg>
+						</button>
+						<!-- Mobile menu dropdown
+						<div class="absolute top-0 left-0 w-full">
+							<div class="p-5 bg-white border rounded shadow-sm">
+								<div class="flex items-center justify-between mb-4">
+									<div>
+										<a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
+											<svg class="w-8 text-deep-purple-accent-400" viewBox="0 0 24 24" stroke-linejoin="round" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" stroke="currentColor" fill="none">
+												<rect x="3" y="1" width="7" height="12"></rect>
+												<rect x="3" y="17" width="7" height="6"></rect>
+												<rect x="14" y="1" width="7" height="6"></rect>
+												<rect x="14" y="11" width="7" height="12"></rect>
+											</svg>
+											<span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">Company</span>
+										</a>
+									</div>
+									<div>
+										<button aria-label="Close Menu" title="Close Menu" class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+											<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+												<path
+													fill="currentColor"
+													d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+												></path>
+											</svg>
+										</button>
+									</div>
+								</div>
+								<nav>
+									<ul class="space-y-4">
+										<li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Product</a></li>
+										<li><a href="/" aria-label="Our product" title="Our product" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Features</a></li>
+										<li><a href="/" aria-label="Product pricing" title="Product pricing" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Pricing</a></li>
+										<li><a href="/" aria-label="About us" title="About us" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">About us</a></li>
+										<li><a href="/" aria-label="Sign in" title="Sign in" class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">Sign in</a></li>
+										<li>
+											<a
+												href="/"
+												class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+												aria-label="Sign up"
+												title="Sign up"
+											>
+												Sign up
+											</a>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+						-->
+					</div>
+				</navbar>
+			</x-wrapper>
+		</header>
+		
 {{--		<header>--}}
 {{--			<ul class="flex justify-between">--}}
 {{--				<li>--}}
