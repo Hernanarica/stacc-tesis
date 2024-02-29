a756ef66a9dc07a90435af237f60c8e6e7992803
Hernán Arica <hernodev@gmail.com>
2024-02-29 19:50:17 -0300

    feat: se renueva el formulario de contacto

--- resources/views/sections/contact.blade.php
+++ resources/views/sections/contact.blade.php
@@ -1,55 +1,79 @@
 @extends('layout.layout')
 @section('title', 'Contacto')
 @section('content')
-		<x-wrapper>
-			<div class="min-h-[calc(100vh-84px)] relative bg-white">
-				<div class="absolute inset-0">
-					<div class="absolute inset-y-0 left-0 w-1/2"></div>
-				</div>
-				<div class="relative mx-auto max-w-7xl lg:grid lg:grid-cols-5">
-					<div class="py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
-						<div class="mx-auto max-w-lg">
-							<h2 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Contactanos</h2>
-							<p class="mt-3 text-lg leading-6 text-gray-500">Envianos un email con tu consulta y nuestro equipo te responderá a la brevedad, saludos!</p>
-						</div>
-					</div>
-					<div class="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
-						<div class="mx-auto max-w-lg lg:max-w-none">
-							<form
-								action="#"
-								method="POST"
-								class="grid grid-cols-1 gap-y-6"
-								id="form"
-							>
-								<div>
-									<label for="name" class="sr-only">Nombre</label>
-									<input type="text" name="name" id="name" autocomplete="name" class="block w-full bg-white border py-3 px-4 border-gray-300 rounded-md focus:border-stacc-purple focus:ring-stacc-purple
-										focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Nombre">
-								</div>
-								<div>
-									<label for="email" class="sr-only">Email</label>
-									<input id="email" name="email" type="email" autocomplete="email" class="block w-full bg-white border py-3 px-4 border-gray-300 rounded-md focus:border-stacc-purple focus:ring-stacc-purple
-										focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email">
-								</div>
-								<div>
-									<label for="phone" class="sr-only">Teléfono</label>
-									<input type="text" name="phone" id="phone" autocomplete="tel" class="block w-full bg-white border py-3 px-4 border-gray-300 rounded-md focus:border-stacc-purple focus:ring-stacc-purple
-										focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Teléfono">
-								</div>
-								<div>
-									<label for="message" class="sr-only">Mensaje</label>
-									<textarea id="message" name="message" rows="4" class="block w-full bg-white border py-3 px-4 border-gray-300 rounded-md focus:border-stacc-purple focus:ring-stacc-purple
-										focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Mensaje"></textarea>
-								</div>
-								<div>
-									<button type="submit" class="px-8 py-3 w-fit text-sm font-medium tracking-wide text-white bg-gradient-to-r from-stacc-purple to-stacc-red
-				rounded-md focus:opacity-90 focus:outline-none">Submit</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</x-wrapper>
+  <section>
+    <div class="relative isolate bg-white">
+      <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+        <div class="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
+          <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
+            <p class="mt-6 text-lg leading-8 text-gray-600">Proin volutpat consequat porttitor cras nullam gravida at. Orci molestie a eu arcu. Sed ut tincidunt integer elementum id sem. Arcu sed malesuada et magna.</p>
+            <dl class="mt-10 space-y-4 text-base leading-7 text-gray-600">
+              <div class="flex gap-x-4">
+                <dt class="flex-none">
+                  <span class="sr-only">Address</span>
+                  <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
+                  </svg>
+                </dt>
+                <dd>545 Mavis Island<br>Chicago, IL 99191</dd>
+              </div>
+              <div class="flex gap-x-4">
+                <dt class="flex-none">
+                  <span class="sr-only">Telephone</span>
+                  <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                  </svg>
+                </dt>
+                <dd><a class="hover:text-gray-900" href="tel:+1 (555) 234-5678">+1 (555) 234-5678</a></dd>
+              </div>
+              <div class="flex gap-x-4">
+                <dt class="flex-none">
+                  <span class="sr-only">Email</span>
+                  <svg class="h-7 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                  </svg>
+                </dt>
+                <dd><a class="hover:text-gray-900" href="mailto:hello@example.com">hello@example.com</a></dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+        <form action="#" method="POST" class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+          <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+            <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">Nombre</label>
+                <div class="mt-2.5">
+                  <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+              </div>
+              <div>
+                <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Teléfono</label>
+                <div class="mt-2.5">
+                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+              </div>
+              <div class="sm:col-span-2">
+                <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                <div class="mt-2.5">
+                  <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+              </div>
+              <div class="sm:col-span-2">
+                <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">Mensaje</label>
+                <div class="mt-2.5">
+                  <textarea name="message" id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                </div>
+              </div>
+            </div>
+            <div class="mt-8 flex justify-end">
+              <button type="submit" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 @endsection
 @vite('./resources/js/emailContact.js')
