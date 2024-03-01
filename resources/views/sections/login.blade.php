09fec8d59eced2e2266a484350b4378672691572
Hernán Arica <hernodev@gmail.com>
2024-03-01 08:35:11 -0300

    fix: mejora de UI en el detalle de local

--- resources/views/sections/login.blade.php
+++ resources/views/sections/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.layout')
 @section('title', 'Inicia sesion')
 @section('content')
-  <section class="flex min-h-[calc(100vh-80px)] flex-col justify-center px-6 py-12 lg:px-8 border">
+  <section class="flex min-h-[calc(100vh-80px)] flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
         account</h2>
