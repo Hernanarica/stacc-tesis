2e80ce751396396e1b02ae17b9dfb4e962705565
Hernán Arica <hernodev@gmail.com>
2024-03-01 09:58:48 -0300

    feat: se renuava la UI de la sección locales favoritos

--- resources/views/sections/my-favorites.blade.php
+++ resources/views/sections/my-favorites.blade.php
@@ -1,35 +1,81 @@
 <?php
+  use \Illuminate\Support\Carbon;
 /** @var \ChristianKuri\LaravelFavorite\Models\Favorite $favorites */
+
+Carbon::setLocale('es');
+
+$currentDay = now()->dayName;
 ?>
 @extends('layout.layout')
 @section('title', 'Mis favoritos')
 @section('content')
-	<x-wrapper>
-		<h2 class="text-3xl font-bold text-gray-700">Mis Favotiros</h2>
-		<div class="min-h-[calc(100vh-140px)] mx-auto max-w-2xl py-12 sm:py-14 lg:max-w-8xl">
-			@if($favorites->count() > 0)
-				<div class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
-					@foreach($favorites as $favorite)
-						<a
-							href="{{ route('locals.show', ['local' => $favorite->id]) }}"
-							aria-label="ver información sobre {{ $favorite->name }}"
-							class="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-						>
-							<img class="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-							     src="{{ url('uploads/images/local/' . $favorite->image) }}" alt="">
-							<div class="flex flex-col justify-between p-4 leading-normal">
-								<h3
-									class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $favorite->name }}</h3>
-								<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Abierto de {{ $favorite->opening_time }}
-									a {{ $favorite->closing_time }}</p>
-								<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Dirección:{{ $favorite->address }}</p>
-							</div>
-						</a>
-					@endforeach
-				</div>
-			@else
-				<h3 class="text-3xl font-bold text-gray-700 text-center mx-auto w-full">No tienes favoritos por ahora.</h3>
-			@endif
-		</div>
-	</x-wrapper>
+  <section class="mx-auto min-h-[calc(100vh-80px)] max-w-7xl px-4 sm:px-6 lg:px-8">
+    @if($favorites->isNotEmpty())
+      <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        @foreach($favorites as $favorite)
+          {{--            {{ dd($favorite->schedules['viernes']) }}--}}
+          <li class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow">
+            <div class="flex flex-1 flex-col p-8">
+              <img
+                  class="mx-auto h-32 w-32 flex-shrink-0 rounded-full"
+                  src="{{ asset('uploads/images/local/' . $favorite['cover-photo']) }}"
+                  alt="{{ $favorite->name }}"
+              >
+              <h3 class="mt-6 text-sm font-medium text-gray-900">{{ $favorite->name }}</h3>
+              <dl class="mt-1 flex flex-grow flex-col justify-between">
+                <dt class="sr-only">Title</dt>
+                <dd class="text-sm text-gray-500">{{ $favorite->name }} ({{ $favorite->neighborhood->name }})</dd>
+                <dt class="sr-only">Role</dt>
+                <dd class="mt-3">
+                    <span class="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                      Abierto
+{{--                      Abierto {{ $favorite->schedules['viernes']["$currentDay-closing-time"] }}--}}
+                    </span>
+                </dd>
+              </dl>
+            </div>
+            <div>
+              <div class="-mt-px flex divide-x divide-gray-200">
+                <div class="flex w-0 flex-1">
+                  <a href="mailto:janecooper@example.com" class="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-gray-400">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+                    </svg>
+                    Web
+                  </a>
+                </div>
+                <div class="-ml-px flex w-0 flex-1">
+                  <a href="tel:{{ $favorite->phone }}" class="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900">
+                    <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
+                    </svg>
+                    Llamar
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+        @endforeach
+      </ul>
+    @else
+      <div class="rounded-md bg-blue-50 p-4">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <svg class="h-5 w-5 text-blue-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z" clip-rule="evenodd" />
+            </svg>
+          </div>
+          <div class="ml-3 flex-1 md:flex md:justify-between">
+            <p class="text-sm text-blue-700">No tenés favoritos por ahora.</p>
+            <p class="mt-3 text-sm md:ml-6 md:mt-0">
+              <a href="{{ route('locals.index') }}" class="whitespace-nowrap font-medium text-blue-700 hover:text-blue-600">
+                Agregar
+                <span aria-hidden="true"> &rarr;</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    @endif
+  </section>
 @endsection
