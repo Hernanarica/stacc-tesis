4601b1fc894e601455813f9531c8fc0abdb730bc
Hernán Arica <hernodev@gmail.com>
2024-03-01 09:15:44 -0300

    feat: se renuav al UI de la seccion pedido de baja del local

--- resources/views/sections/stores-delete.blade.php
+++ resources/views/sections/stores-delete.blade.php
@@ -1,37 +1,32 @@
 <?php
 /** @var \App\Models\Local[] $local */
-
 ?>
 @extends('layout.layout')
 @section('title', 'Mis Locales | Pedir baja')
 @section('content')
-	<x-wrapper>
-		<div class="text-center mb-6">
-			<h2 class="text-4xl font-bold text-center text-gray-700">Pedido de Eliminar</h2>
-		</div>
-		<div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl mx-auto">
-			<img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="{{ url('/uploads/images/local/' . $local->image)}}" alt="{{$local->image_alt}}">
-			<div class="flex flex-col justify-between p-4 leading-normal">
-				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{$local->name}}</h5>
-				<p class="mb-3 font-normal text-gray-700 ">{{$local->address}}</p>
-			</div>
-		</div>
-		<div class="flex flex-col items-center justify-center w-full mt-4">
-			<form action="" method="POST">
-				<div class="flex flex-col items-center justify-center w-full mt-4">
-					<div class="flex flex-col items-center justify-center w-full mt-4">
-						<label class="block mb-2 text-sm font-bold text-gray-700" for="name">
-							¿Por qué quieres dar de baja tu local?
-						</label>
-						<textarea class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" cols="58" rows="4" name="reason" required></textarea>
-					</div>
-					<div class="flex flex-col items-center justify-center w-full mt-4">
-						<button class="px-4 py-2 font-bold text-white bg-red-500 rounded-full hover:bg-red-700 focus:outline-none focus:shadow-outline" type="submit">
-							Pedir baja
-						</button>
-					</div>
-				</div>
-			</form>
-		</div>
-	</x-wrapper>
+  <section class="mx-auto min-h-[calc(100vh-80px)] max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div class="mx-auto max-w-2xl text-center">
+        <p class="text-base font-semibold leading-7 text-indigo-600">Te ayudamos</p>
+        <h2 class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Solicitud de baja</h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600">
+          <b>Queremos ayudarte a gestionar la baja de tu local({{ $local->name }}).</b>
+          <br>
+          Tu opinión es muy importante para nosotros y nos ayudará a mejorar nuestros servicios en el futuro.
+          Por favor, toma unos minutos para completar el siguiente formulario y cuéntanos en detalle las razones por las que deseas dar de baja tu local.
+        </p>
+      </div>
+    </div>
+    <form action="" method="post" class="max-w-lg mx-auto space-y-5">
+      <div>
+        <label for="comment" class="block text-sm font-medium leading-6 text-gray-900">Contanos más</label>
+        <div class="mt-2">
+          <textarea rows="4" name="comment" id="comment" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+        </div>
+      </div>
+      <div>
+        <button type="button" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Enviar</button>
+      </div>
+    </form>
+  </section>
 @endsection
